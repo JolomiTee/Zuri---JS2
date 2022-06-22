f31be41709bd3d94ecf2bca50d6814d31523e8ec
Taiwo Jolomi <taiwojolomi15@gmail.com>
2022-06-22 19:15:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var yourName = prompt("Hi what's your name? ");
+var height = prompt("And what's your height? Don't worry you can estimate..");
+var nation = prompt("Finally, what nationality are you? ");
+
+var msg = 'Your name is ' + yourName + ', <br/>Your height is ' + height + ', <br/>You are from ' + nation;
+
+document.write('<h1>' + msg + '</h1>');
